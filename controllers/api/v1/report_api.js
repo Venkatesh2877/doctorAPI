4af00e76263b9acd7f16a80f09cb598fca2f2bd2
Venkatesh2877 <venky2877@gmail.com>
2023-08-25 22:52:40 +0530

    all the reports of all the patients filtered by a specific status

---
+++ controllers/api/v1/report_api.js
@@ -0,0 +1,19 @@
+const Reports=require('../../../models/report')
+
+module.exports.allStatusReport= async function(req,res){
+     try{
+        let allStatusReports= await Reports.find({Status: req.params.status});
+
+        return res.json(200, {
+            message:`all the reports of all the patients filtered by a specific status - ${req.params.status}`,
+            data:{
+                reports:allStatusReports
+            }
+        });
+    }catch(err){
+        console.log(err);
+        return res.json(500, {
+            message:`Patient Register error ${err}`
+        })
+    }
+}
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,7 +1,11 @@
 const express=require('express');
 const router=express.Router();
+const passport=require('passport');
+const reportApi= require('../../../controllers/api/v1/report_api');
 
 router.use('/doctors', require('./doctors'));
 router.use('/patients', require('./patients'));
+router.get('/reports/:status',passport.authenticate('jwt',{session:false}),reportApi.allStatusReport);
+
 
 module.exports= router;
