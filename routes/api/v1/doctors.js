b84ea9d9deed97188114633678ac84237d1248a7
Venkatesh2877 <venky2877@gmail.com>
2023-08-25 19:57:29 +0530

    server setup done, doctor register and login done

--- routes/api/v1/doctors.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router=express.Router();
-
-const doctorApi= require('../../../controllers/api/v1/doctor_api');
-
-router.post('/register', doctorApi.register);
-router.get('/login', doctorApi.login);
-
-module.exports= router;
